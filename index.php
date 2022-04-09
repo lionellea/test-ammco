7231261fa2d8cb1e43d1b3faea9116ed74066dac
lionellea <44930041+lionellea@users.noreply.github.com>
2022-04-09 15:24:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,10 +11,9 @@
 	 <body style="margin-top: 200px;margin-left:200px;text-align:justify">
 	   
 		<h3>
-			Le film le plus populaire de l'utilisateur 
+			Le film le plus populaire dans le réseau d'amis de l'utilisateur 
 			<?php echo  $user2->getNom(); ?>
-			
-			dans son réseau d'amis est : <?php echo meilleurFilm($user2);?>
+			est : <?php echo meilleurFilm($user2);?>
 			</h3>
 	 
 		
